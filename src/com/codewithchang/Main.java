f0d097744fa766a286e5754126477881629c7dce
Chang <cnxiong03@gmail.com>
2021-03-28 23:57:59 -0400

    solving getDaysInMonth method

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,45 +3,29 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-        printNumberInWord(-9);
     }
 
-    private static void printNumberInWord (int number) {
-        switch (number) {
-            case 0:
-                System.out.println("ZERO");
-                break;
-            case 1:
-                System.out.println("ONE");
-                break;
-            case 2:
-                System.out.println("TWO");
-                break;
-            case 3:
-                System.out.println("THREE");
-                break;
-            case 4:
-                System.out.println("FOUR");
-                break;
-            case 5:
-                System.out.println("FIVE");
-                break;
-            case 6:
-                System.out.println("SIX");
-                break;
-            case 7:
-                System.out.println("SEVEN");
-                break;
-            case 8:
-                System.out.println("EIGHT");
-                break;
-            case 9:
-                System.out.println("NINE");
-                break;
+    public static boolean isLeapYear(int year) {
 
-            default:
-                System.out.println("OTHER");
+        if(year >= 1 && year <= 9999) {
+            if(year % 4 == 0) {
+                if(year % 100 != 0) {
+                    if(year % 400 == 0) {
+                        return true;
+                    }
+                }
+            }
+
         }
+        return false;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if(month < 1 || month > 12) {
+            return -1;
+        } else if(year < 1 || year > 9999) {
+            return -1;
+        } else if
     }
 
 }
