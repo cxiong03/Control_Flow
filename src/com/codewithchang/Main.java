535b2fcb05d8d94fe03dfc508d3df2c37c55dc4d
Chang <cnxiong03@gmail.com>
2021-03-30 23:54:42 -0400

    still unsolved

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -3,32 +3,35 @@ package com.codewithchang;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(-1600));
     }
 
     public static boolean isLeapYear(int year) {
 
-        if(year >= 1 && year <= 9999) {
-            if(year % 4 == 0) {
-                if(year % 100 != 0) {
-                    if(year % 400 == 0) {
-                        return true;
+        if (year >= 1 && year <= 9999) {
+
+            switch (year) {
+                case 0:
+                    if (year % 4 == 0 || year % 100 != 0 || year % 400 == 0) {
+                        System.out.println("True");
+                        break;
                     }
-                }
+
+                default:
+                    return false;
             }
 
+//
+//    public static int getDaysInMonth(int month, int year) {
+//        if(month < 1 || month > 12) {
+//            return -1;
+//        } else if(year < 1 || year > 9999) {
+//            return -1;
+//        } else if(isLeapYear(year) == true) {
+//
+//        }
+//    }
         }
         return false;
     }
-
-    public static int getDaysInMonth(int month, int year) {
-        if(month < 1 || month > 12) {
-            return -1;
-        } else if(year < 1 || year > 9999) {
-            return -1;
-        } else if(isLeapYear(year) == true) {
-
-        }
-    }
-
 }
