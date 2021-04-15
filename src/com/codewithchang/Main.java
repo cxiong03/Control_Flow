1014002979025e0dcb5bba46bb2635cf564b4781
Chang <cnxiong03@gmail.com>
2021-04-15 00:10:29 -0400

    decrementing forloop

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -6,7 +6,12 @@ public class Main {
 //        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
 
         for(int i = 2; i < 9; i++) {
-            System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+            //"%.2f" converts the number and output with 2 decimal point
+        }
+
+        for(int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
     }
 
